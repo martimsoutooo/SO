27408696e3dc46f31a3274b681813c7056ccfa83
Martim Santos <santos.martim@ua.pt>
2023-09-21 16:27:58 +0100

    first commit

---
+++ Aula02/aula02e03.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -i
+# O comando type permite saber o tipo de um comando
+type rm
+type cd
+type for
+type ll
+type dequote 
+
+#rm is /usr/bin/rm 
+#cd is a shell builtin
+#for is a shell keyword
+#ll: not found
+#dequote: not found 
