be9a98f9567b144468976a27db3eab51a43e44f2
Martim Santos <santos.martim@ua.pt>
2023-10-26 17:21:35 +0100

    começar aula05

--- Aula05/args1.c
+++ Aula05/args1.c
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 3)
+    {
+        printf("ERROR: No arguments provided\n");
+        return EXIT_FAILURE;
+    }
     int i;
 
     for(i = 0 ; i < argc ; i++)
